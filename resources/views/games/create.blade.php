18aa2e6b04d44a3185d5e90c82285804f59b6fea
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2024-06-14 12:05:42 +0200

    De code voor de CREATE (met validatie)

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -1,1 +1,9 @@
 <h1>Maak een game aan</h1>
+<a href="{{ url('/games') }}">Ga terug</a>
+<form action="{{ url('/games') }}" method="post">
+    @csrf
+    <label for="name">Naam van het spel</label>
+    @error('name') {{ $message }} @enderror
+    <input type="text" name="name" id="name" value="{{ old('name') }}">
+    <input type="submit" value="Maak een nieuwe game aan">
+</form>
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,21 @@ Route::get('/', function () {
 
 Route::get('/games', function () {
     $games = Game::all();
-    $games = Game::where(['name' => 'kel'])->get();
     return view('games.index', compact('games'));
 });
+
+Route::post('/games', function (\Illuminate\Http\Request $request) {
+   $game =  $request->validate([
+        'name' => 'required|string|min:5|max:20',
+    ]);
+
+    Game::create($game);
+    return redirect('/games');
+
+
+
+});
+
 
 Route::get('/games/create', function (){
    return view('games.create');
