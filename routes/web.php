0d23b806a79b04b79ba279e44ff800b667da9675
MoroniStubbe <31618299+MoroniStubbe@users.noreply.github.com>
2024-06-19 14:32:33 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,11 @@ use App\Models\Game;
 
 Route::get('/', function () {
     return redirect('games');
-});
+})->name('index');
 
 Route::get('/games', function () {
     $games = Game::all();
     return view('games.index', compact('games'));
-})->name('games');
-
-Route::get('/games/{id}', function ($id) {
-    $games = Game::all();
-    return view('games.show', compact('games'));
 })->name('games');
 
 Route::post('/games', function (\Illuminate\Http\Request $request) {
@@ -23,9 +18,14 @@ Route::post('/games', function (\Illuminate\Http\Request $request) {
     ]);
 
     Game::create($game);
-    return redirect()->route('/games');
+    return redirect()->route('games');
 });
 
 Route::get('/games/create', function () {
     return view('games.create');
-});
+})->name('games.create');
+
+Route::get('/games/{id}', function ($id) {
+    $games = Game::all();
+    return view('games.show', compact('games'));
+})->name('games.show');
